0b85fe541d92af84655ae449036831bb480c87aa
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-06-11 20:11:53 +0300

    feat: Add task creation functionality in app.js

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const app = express();
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/ToDo', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Create a schema
+const schema = new mongoose.Schema({ title: String });
+const Task = mongoose.model('Task', schema);
+
+//insert
+const firstTask = new Task({ title: 'Learn Node.js' });
+firstTask.save().then(() => console.log('new Record inserted'));
