80b32691bc8d122002ef4c7b2d16926713e6e5fa
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-06-11 20:00:14 +0300

    chore: Connect to MongoDB in app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/ToDo', { useNewUrlParser: true, useUnifiedTopology: true });
